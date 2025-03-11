fa624c61dd8542f1365ac5781c72a642949704f6
Ruslana Onyshchuk <lalelya7@gmail.com>
2025-03-11 09:44:23 +0100

    made small changes

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,11 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import apiRouter from "./mealSharing.js"; 
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use("/api", apiRouter);
 
