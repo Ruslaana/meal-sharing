0767c7b8b3a849e3d3155fe5861f93ff89294fd1
Ruslana Onyshchuk <lalelya7@gmail.com>
2025-04-13 13:01:43 +0200

    update frontend

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import mealsRouter from './routers/meals.js';
-import reservationsRouter from './routers/reservation.js';
+import reservationsRouter from './routers/reservations.js';
 import reviewsRouter from './routers/reviews.js';
 
 const app = express();
